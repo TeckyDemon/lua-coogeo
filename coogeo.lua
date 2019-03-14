6638d91de390ee30a506b424f3ad28f3218394e7
Michał Wróblewski <20270277+DeBos99@users.noreply.github.com>
2019-03-14 22:14:02 +0000

    Add files via upload

---
+++ coogeo.lua
@@ -0,0 +1,10 @@
+function math.distance(x1,y1,x2,y2)
+	return math.sqrt((x2-x1)^2+(y2-y1)^2)
+end
+function math.midpoint(x1,y1,x2,y2)
+	return {x=(x1+x2)/2,y=(y1+y2)/2}
+end
+function math.slope()
+	return (y2-y1)/(x2-x1)
+end
+math.gradient=math.slope
